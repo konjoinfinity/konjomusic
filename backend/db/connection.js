74b46700eeaf42477a4c47cc2f4be06146f6a003
Wesley Scholl <wesleyscholl@gmail.com>
2022-06-22 07:18:03 -0600

    Update connection.js
    update connection

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -2,9 +2,21 @@ const mongoose = require("mongoose");
 mongoose.Promise = Promise;
 
 if (process.env.NODE_ENV == "production") {
-  mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
+  mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  });
+  console.log("Production Database Connection Successful");
 } else {
-  mongoose.connect("mongodb://localhost/konjomusic", { useNewUrlParser: true });
+  mongoose.connect("mongodb://localhost/konjomusic", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  });
+  console.log("Development Database Connection Successful");
 }
 
 module.exports = mongoose;
