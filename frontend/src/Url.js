b85e4336952909a92bef1a4b76010c479691c805
Wesley Scholl <clients@konjoweb.com>
2022-11-23 16:15:15 -0500

    added deployed url to readme

--- frontend/src/Url.js
+++ frontend/src/Url.js
@@ -1,5 +1,5 @@
 const backendUrl = window.location.host.includes("localhost")
   ? "http://localhost:4000/"
-  : "https://music-backend.onrender.com";
+  : "https://music-backend.onrender.com/";
 
 export default backendUrl;
